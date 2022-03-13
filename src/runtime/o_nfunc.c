4f8aaa7eca997e159d387c7d5ec8c22ae92221e0
cozis <heibyob@gmail.com>
2022-03-13 16:05:34 +0100

    cuter license comment

--- src/runtime/o_nfunc.c
+++ src/runtime/o_nfunc.c
@@ -1,20 +1,31 @@
 
-/* Copyright (c) 2022 Francesco Cozzuto <francesco.cozzuto@gmail.com>
-**
-** This file is part of The Noja Interpreter.
-**
-** The Noja Interpreter is free software: you can redistribute it and/or
-** modify it under the terms of the GNU General Public License as published
-** by the Free Software Foundation, either version 3 of the License, or (at 
-** your option) any later version.
-**
-** The Noja Interpreter is distributed in the hope that it will be useful, 
-** but WITHOUT ANY WARRANTY; without even the implied warranty of 
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
-** Public License for more details.
-**
-** You should have received a copy of the GNU General Public License along 
-** with The Noja Interpreter. If not, see <http://www.gnu.org/licenses/>.
+/* +--------------------------------------------------------------------------+
+** |                          _   _       _                                   |
+** |                         | \ | |     (_)                                  |
+** |                         |  \| | ___  _  __ _                             |
+** |                         | . ` |/ _ \| |/ _` |                            |
+** |                         | |\  | (_) | | (_| |                            |
+** |                         |_| \_|\___/| |\__,_|                            |
+** |                                    _/ |                                  |
+** |                                   |__/                                   |
+** +--------------------------------------------------------------------------+
+** | Copyright (c) 2022 Francesco Cozzuto <francesco.cozzuto@gmail.com>       |
+** +--------------------------------------------------------------------------+
+** | This file is part of The Noja Interpreter.                               |
+** |                                                                          |
+** | The Noja Interpreter is free software: you can redistribute it and/or    |
+** | modify it under the terms of the GNU General Public License as published |
+** | by the Free Software Foundation, either version 3 of the License, or (at |
+** | your option) any later version.                                          |
+** |                                                                          |
+** | The Noja Interpreter is distributed in the hope that it will be useful,  |
+** | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+** | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General |
+** | Public License for more details.                                         |
+** |                                                                          |
+** | You should have received a copy of the GNU General Public License along  |
+** | with The Noja Interpreter. If not, see <http://www.gnu.org/licenses/>.   |
+** +--------------------------------------------------------------------------+ 
 */
 
 /*
