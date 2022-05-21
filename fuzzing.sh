784914c7e62ba24e64f6a447a7c7e3124debc819
cozis <heibyob@gmail.com>
2022-05-21 23:01:51 +0200

    changed the fuzzing script to use the makefile

--- fuzzing.sh
+++ fuzzing.sh
@@ -1,4 +1,4 @@
-./build.sh --fuzzing
+make CC=afl-clang-fast
 export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
 export AFL_SKIP_CPUFREQ=1
 afl-fuzz -i examples/ -o out -m none -d -- ./build/noja run @@
